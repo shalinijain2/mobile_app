f6deff0589cd9b47543140cdd624d2cfc7865231
shalinijain2 <69742562+shalinijain2@users.noreply.github.com>
2020-08-15 21:43:07 -0700

    add mobile app files

---
+++ KothariEnterprises/app/src/main/java/com/example/kotharienterprises/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.kotharienterprises
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+public class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
